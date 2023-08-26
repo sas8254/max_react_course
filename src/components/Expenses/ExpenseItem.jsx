590b73bff26b34f0ada2aa1c4c140d997609b967
sas8254 <shashibhushansingh11@gmail.com>
2023-08-26 16:29:03 +0530

    72. Adding Form Inputs.

--- src/components/Expenses/ExpenseItem.jsx
+++ src/components/Expenses/ExpenseItem.jsx
@@ -1,14 +1,20 @@
+import { useState } from "react";
 import Card from "../UI/Card";
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 export default function ExpenseItem(props) {
+  const [title, setTitle] = useState(props.title);
+  function clickHandler() {
+    setTitle("Updated");
+  }
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item_description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item_price">${props.amount}</div>
+        <button onClick={clickHandler}>Change Title</button>
       </div>
     </Card>
   );
---
+++ src/components/NewExpense/NewExpense.jsx
@@ -0,0 +1,10 @@
+import ExpenseForm from "./ExpenseForm";
+import "./NewExpense.css";
+
+export default function NewExpenses() {
+  return (
+    <div className="new-expense">
+      <ExpenseForm />
+    </div>
+  );
+}
