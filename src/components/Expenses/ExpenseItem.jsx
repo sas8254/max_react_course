45bfb8bf87675ba66087a36a0e71b5a68c3b7d01
sas8254 <shashibhushansingh11@gmail.com>
2023-09-14 15:29:04 +0530

    86. Rendering Lists of Data.

--- src/components/Expenses/ExpenseItem.jsx
+++ src/components/Expenses/ExpenseItem.jsx
@@ -4,17 +4,12 @@ import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 export default function ExpenseItem(props) {
-  const [title, setTitle] = useState(props.title);
-  function clickHandler() {
-    setTitle("Updated");
-  }
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item_description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item_price">${props.amount}</div>
-        <button onClick={clickHandler}>Change Title</button>
       </div>
     </Card>
   );
